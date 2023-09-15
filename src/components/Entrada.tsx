cb2e356dab91a0f2ce0f8c95c84e8fc32039fd29
Eduardo Xavier <49372451+Eduardo-XavierPaula@users.noreply.github.com>
2023-09-15 01:25:18 -0300

    :hammer: Construindo logica de formulário
    -Navegação entre tabela e formulário.
    -Tipo de formulário, novo ou editar cliente.

---
+++ src/components/Entrada.tsx
@@ -0,0 +1,31 @@
+interface EntradaProps {
+    texto: string
+    valor: any
+    tipo?: 'text' | 'number'
+    somenteLeitura?: boolean
+    className?: string
+    valorMudou?: (valor: any) => void
+}
+
+export default function Entrada(props: EntradaProps){
+    return(
+        <div className={`flex flex-col ${props.className}`}>
+            <label className={`
+                mb-2
+            `}>
+                {props.texto}
+            </label>
+            <input
+                type={props.tipo ?? 'text'}
+                value={props.valor}
+                readOnly={props.somenteLeitura}
+                className={`
+                    border border-purple-500 rounded-lg
+                    focus:outline-none bg-gray-100 ${props.somenteLeitura? '' : 'focus:bg-white'}
+                    px-4 py-2
+                `}
+                onChange={e => props.valorMudou?.(e.target.value)}
+            />
+        </div>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
 import Botao from "../components/Botao";
+import Formulario from "../components/Formulario";
 import Layout from "../components/Layout";
 import Tabela from "../components/Tabela";
 import Cliente from "../core/Client";
 
 export default function Home() {
 
+  const [cliente, setCliente] = useState<Cliente>(Cliente.vazio())
+  const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
   const clientes = [
     new Cliente('Ana', 34, '1'),
     new Cliente('Bia', 45, '2'),
@@ -12,12 +16,26 @@ export default function Home() {
     new Cliente('Pedro', 54, '4')
   ]
 
-  function clienteSelecionado(cliente: Cliente){
-    console.log(cliente.nome)
+
+
+  function clienteSelecionado(cliente: Cliente) {
+    setCliente(cliente)
+    setVisivel('form')
   }
-  function clienteExcluido(cliente: Cliente){
+
+  function clienteExcluido(cliente: Cliente) {
     console.log(`Excluir ${cliente.nome}`)
   }
+
+  function novoCliente() {
+    setCliente(Cliente.vazio())
+    setVisivel('form')
+  }
+  function salvarCliente(cliente: Cliente) {
+    console.log(cliente)
+    setVisivel('tabela')
+  }
+
 
   return (
     <div className={`
@@ -26,13 +44,26 @@ export default function Home() {
       text-white
     `}>
       <Layout titulo="Cadastro Simples">
-        <div className="flex justify-end">
-          <Botao cor="green" className="mb-4">Novo Cliente</Botao>
-        </div>
-        <Tabela clientes={clientes}
-          clienteSelecionado={clienteSelecionado}
-          clienteExcluido={clienteExcluido}
-        ></Tabela>
+        {visivel === 'tabela' ? (
+          <>
+            <div className="flex justify-end">
+              <Botao cor="green" className="mb-4"
+                onClick={novoCliente}>
+                  Novo Cliente
+                </Botao>
+            </div>
+            <Tabela clientes={clientes}
+              clienteSelecionado={clienteSelecionado}
+              clienteExcluido={clienteExcluido}
+            ></Tabela>
+          </>
+        ) : (
+          <Formulario
+            cliente={cliente}
+            cancelado={()=>setVisivel('tabela')}
+            clienteMudou={salvarCliente}
+          ></Formulario>
+        )}
       </Layout>
     </div>
   )
